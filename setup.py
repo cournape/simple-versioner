354af6f0306e44f6b48643fa277b17d3fdd9f629
David Cournapeau <cournape@gmail.com>
2017-08-25 11:41:55 +0900

    MAINT: README.md -> README.rst.
    setuptools does not understand markdown. Also fixed the README content
    to reflect the updated filename.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name="simple-versioner",
+    # Because I want to keep versioner a single module to be copied over, I
+    # sadly cannot use simple-versioner to version simpler-versioner :)
+    version="0.1.0.dev0",
+    author="David Cournapeau",
+    author_email="cournape@gmail.com",
+    description="Simple versioning module for python packages.",
+    license="MIT",
+    url="https://github.com/cournape/simple-versioner",
+)
